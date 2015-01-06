c3b423341fa34396a293e876c8d9000cd0897042
firefly1 <netohi@web.de>
2015-01-06 23:38:14 +0100

    .

---
+++ runAnalysis.R
@@ -0,0 +1,9 @@
+﻿run.analysis(form, covariates, FDR = 0.1, norm.post.repl = FALSE, 
+             norm.peaks = c("common", "all", "none"), normalization, 
+             add.norm = TRUE,  repl.method = "max", use.model = "lm",
+             pval.fcn = "default", lrg.only = TRUE, masses = NA,
+             isotope.dist = 7, root.dir = ".", lrg.dir,
+             lrg.file = lrg_peaks.RData, res.dir,
+             res.file = "analyzed.RData", overwrite = FALSE,
+             use.par.file = FALSE, par.file = "parameters.RData",
+             bhbysubj = TRUE, subs, ...)
